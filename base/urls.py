4dcb34d16f553e6fb5fe6d1380e75070894a2081
AnggaPutraa <yananggaa@gmail.com>
2022-12-24 23:31:26 +0700

    refactor: config endpoints

---
+++ base/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from base.views import *
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('api/register/', RegisterUser.as_view(), name="register"),
+    path('api/login/', LoginUser.as_view(), name='login'),
+    path('api/logout/', LogoutUser.as_view(), name='logout'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+]
