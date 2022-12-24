5b289bb8f02367023710460b6227445ce982e080
AnggaPutraa <yananggaa@gmail.com>
2022-12-24 23:27:21 +0700

    refactor: register, login, logout api views

--- base/views.py
+++ base/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+from base.models import *
+from base.serializers import *
+
+class RegisterUser(APIView):
+    serializer_class = RegisterSerializer
+    def post(self, request):
+        user=request.data
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        return Response(user_data, status=status.HTTP_201_CREATED)
+
+class LoginUser(APIView):
+    serializer_class = LoginSerializer
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+class LogoutUser(APIView):
+    serializer_class = LogoutSerializer
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
