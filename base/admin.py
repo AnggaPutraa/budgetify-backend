10b7c57c956985867a65ea9b6db3c71ab5c5cf2c
AnggaPutraa <yananggaa@gmail.com>
2022-12-25 01:15:51 +0700

    feat: transaction object relationship

--- base/admin.py
+++ base/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from base.views import *
 
 admin.site.register(User)
+admin.site.register(TransactionType)
+admin.site.register(TransactionCategory)
+admin.site.register(TransactionModel)
--- base/models.py
+++ base/models.py
@@ -12,3 +12,29 @@ class User(AbstractUser):
         }
     def __str__(self):
         return self.username
+
+class TransactionType(models.Model):
+    type = models.CharField(max_length=20)
+    class Meta:
+        verbose_name_plural = 'Transaction Type'
+    def __str__(self):
+        return self.type
+
+class TransactionCategory(models.Model):
+    name = models.CharField(max_length=20)
+    type = models.ForeignKey(TransactionType, related_name='transaction_sub_category', on_delete=models.CASCADE)
+    class Meta:
+        verbose_name_plural = 'Transaction Categories'
+    def __str__(self):
+        return self.name
+
+class TransactionModel(models.Model):
+    user = user = models.ForeignKey(User, on_delete=models.CASCADE)
+    type = models.ForeignKey(TransactionType, related_name='transaction', on_delete=models.CASCADE)
+    category = models.ForeignKey(TransactionCategory, related_name='transaction', on_delete=models.CASCADE)
+    note = models.TextField(max_length=200, blank=True)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    class Meta:
+        verbose_name_plural = 'Transactions'
+    def __str__(self):
+        return self.type
