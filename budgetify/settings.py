953ea44ae839f055c842e3ba27401bcd2a453bb1
AnggaPutraa <yananggaa@gmail.com>
2022-12-28 23:45:34 +0700

    refactor: change debug to True

--- budgetify/settings.py
+++ budgetify/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xrv5hx(ybemgnzr0zg!-6a@xzn$y+f0@^qsi-c!z-^rk1^x60%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
