02d6c04b3b3dfb10c184cd70aabcca66744ea7a5
AnggaPutraa <yananggaa@gmail.com>
2022-12-28 23:38:21 +0700

    refactor: remove jezzadmin

--- budgetify/settings.py
+++ budgetify/settings.py
@@ -33,7 +33,6 @@ AUTH_USER_MODEL = "base.User"
 # Application definition
 
 INSTALLED_APPS = [
-    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
